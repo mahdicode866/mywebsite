a236dfaed92fc410746c22353005338105696305
mahdicode866 <mis861400@gmail.com>
2024-08-22 12:07:53 +0330

    this is a new change in  my web site
    this is my change

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from  django.urls import path
 from . import views
 urlpatterns = [
-    path("welcome",views.welcome),
-    path("hello", views.hello)
+    # path("welcome",views.welcome),
+    # path("", views.hello)
+    # path("hello",views.hello)
+    path("",views.mahdi),
+    
     ]
+
--- app/views.py
+++ app/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import  render
 from django.http import HttpResponse
-# def hello(request): 
-#     return  HttpResponse("HELLO WOLRD")
 
-# def goodbey(request):
-#     return HttpResponse("good bey ")
-
-# def belakh(request):
-#     return HttpResponse("bea bokhoreeesh ")
-
-def welcome(request):
-    return HttpResponse("welcome to our web site master here we have some informatin about this world")
-def hello (request):
+# def welcome(request):
+#     return HttpResponse("welcome to our web site master here we have some informatin about this world")
+# def hello (request):
+#     return render(request,'index.html')
+# def hello(request):
+#     return render (request,'index.html')
+def mahdi(request):
     return render(request,'index.html')
+    
